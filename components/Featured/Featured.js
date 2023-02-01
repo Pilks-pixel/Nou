696f743511396442b82b4ec8e0be206b923d953d
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2023-02-01 16:42:31 +0700

    image fix

--- components/Featured/Featured.js
+++ components/Featured/Featured.js
@@ -1,130 +1,114 @@
-import styles from './featured.module.css'
-import utilStyles from '../../styles/utils.module.css'
-import Image from 'next/legacy/image'
+import styles from "./featured.module.css";
+import Image from "next/image";
+import Link from "next/link";
 import React, { useRef, useState, useEffect } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { Navigation, Pagination, Mousewheel } from 'swiper';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Mousewheel, A11y, Keyboard } from "swiper";
 
 const Featured = () => {
 
-      const [sliderOptions, setSliderOptions] = useState(null);
+	const [sliderOptions, setSliderOptions] = useState(null);
 
-      useEffect(() => {
-        const deviceBreakpoints = {
-          480: {
-            slidesPerView: 2,
-            spaceBetween: 15,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 30,
-          }}
+	useEffect(() => {
+		const deviceBreakpoints = {
+			480: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+			768: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			1024: {
+				slidesPerView: 4,
+				spaceBetween: 30,
+			},
+		};
 
-        setSliderOptions(deviceBreakpoints);
-        console.log({sliderOptions}, 'from use effect')
-      },[])
+		setSliderOptions(deviceBreakpoints);
+		console.log({ sliderOptions }, "from use effect");
+	}, []);
 
-      const projectSlider = {
-        className:"slider",
-        modules:[Navigation, Pagination, Mousewheel],
-        spaceBetween:10,
-        slidesPerView:1,
-        loop:true,
-        centeredSlides:true,
-        direction:"horizontal",
-        mousewheel:true,  
-        pagination:{ 
-          clickable: true,
-          dynamicBullets: true 
-        },
-        breakpoints:sliderOptions,
-        scrollbar:{draggable: true }
-      } 
-  return (
-    <>
-    {sliderOptions && <Swiper {...projectSlider}
-      
-      >
-      
-        <SwiperSlide>
-          <div className={styles.projectContainer}>
-            <div className={styles.imageWrapper}>
-              <Image 
-              src='/fresh_kind.png' 
-              height={430} 
-              width={270} 
-              />
-            </div>  
-            <span className={styles.projectName}>silk & sense</span>
-            <span className={styles.projectType}>logo</span>
+	const projectSlider = {
+		className: "slider",
+		modules: [Navigation, Pagination, Mousewheel, A11y, Keyboard],
+		spaceBetween: 10,
+		slidesPerView: 1,
+		loop: true,
+		centeredSlides: true,
+		direction: "horizontal",
+		mousewheel: true,
+		keyboard: { enabled: true },
+		// pagination:{
+		//   clickable: true,
+		//   dynamicBullets: true
+		// },
+		breakpoints: sliderOptions,
+	};
+	return (
+		<>
+			{sliderOptions && (
+				<Swiper {...projectSlider}>
+					<SwiperSlide>
+						<div className={styles.projectContainer}>
+							<Link href='/work'>
+								<Image
+									className={styles.projectImg}
+									src='/fresh_kind.png'
+									height={430}
+									width={270}
+								/>
+							</Link>
+							<span className={styles.projectName}>silk & sense</span>
+							<span className={styles.projectType}>logo</span>
+						</div>
+					</SwiperSlide>
 
-          </div>  
-        </SwiperSlide>
+					<SwiperSlide>
+						<div className={styles.projectContainer}>
+							<Image
+								className={styles.projectImg}
+								src='/silk_28.png'
+								height={430}
+								width={270}
+							/>
+							<span className={styles.projectName}>silk & sense</span>
+							<span className={styles.projectType}>logo</span>
+						</div>
+					</SwiperSlide>
 
-        <SwiperSlide>
-          <div className={styles.projectContainer}>
-            <div className={styles.imageWrapper}>
-              <Image 
-              src='/silk_28.png' 
-              height={430} 
-              width={270} 
-              />
-            </div>  
-            <span className={styles.projectName}>silk & sense</span>
-            <span className={styles.projectType}>logo</span>
+					<SwiperSlide>
+						<div className={styles.projectContainer}>
+							<Image
+								className={styles.projectImg}
+								src='/silk_28.png'
+								height={430}
+								width={270}
+							/>
+							<span className={styles.projectName}>silk & sense</span>
+							<span className={styles.projectType}>logo</span>
+						</div>
+					</SwiperSlide>
 
-          </div>  
-        </SwiperSlide>
-  
-
-        <SwiperSlide>
-          <div className={styles.projectContainer}>
-            <div className={styles.imageWrapper}>
-              <Image 
-              src='/silk_28.png' 
-              height={430} 
-              width={270} 
-              />
-            </div>  
-            <span className={styles.projectName}>silk & sense</span>
-            <span className={styles.projectType}>logo</span>
-
-          </div>  
-        </SwiperSlide>
-  
-
-        <SwiperSlide>
-          <div className={styles.projectContainer}>
-            <div className={styles.imageWrapper}>
-              <Image 
-              src='/silk_28.png' 
-              height={430} 
-              width={270} 
-              />
-            </div>  
-            <span className={styles.projectName}>silk & sense</span>
-            <span className={styles.projectType}>logo</span>
-
-          </div>  
-        </SwiperSlide>
-  
-        
-      
-      </Swiper>}
-  
-  
-    </>
-   
-
-  )
-}
+					<SwiperSlide>
+						<div className={styles.projectContainer}>
+							<Image
+								className={styles.projectImg}
+								src='/silk_28.png'
+								height={430}
+								width={270}
+							/>
+							<span className={styles.projectName}>silk & sense</span>
+							<span className={styles.projectType}>logo</span>
+						</div>
+					</SwiperSlide>
+				</Swiper>
+			)}
+		</>
+	);
+};
 
 export default Featured;
--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,8 @@ import Featured from "../components/featured/featured";
 import Info from "../components/Info/Info";
 import ServiceGrid from "../components/ServiceGrid/ServiceGrid";
 
-
-
 export default function Home() {
-
-
+  
 	return (
 		<div className={utilStyles.container}>
 			<Head>
@@ -22,15 +19,15 @@ export default function Home() {
 			</Head>
 
 			<header className={styles.header}>
-        <div className={styles.headerImage}>
-          <Image
-            src='/noufiles_large.png'
-            alt='nou logo'
-            width={62.5}
-            height={37.5}
-            layout='responsive'
-          />
-        </div>
+				<div className={styles.headerImage}>
+					<Image
+						src='/noufiles_large.png'
+						alt='nou logo'
+						width={62.5}
+						height={37.5}
+						layout='responsive'
+					/>
+				</div>
 				{/* <Nav /> */}
 			</header>
 
@@ -44,7 +41,9 @@ export default function Home() {
 						and websites that elevate and up level your business
 					</span>
 					<div className={utilStyles.pageLink}>
-						<Link href={'/contact'} className={styles.contactLink}>Say Hello</Link>
+						<Link href={"/contact"} className={styles.contactLink}>
+							Say Hello
+						</Link>
 					</div>
 				</section>
 			</main>
@@ -53,42 +52,47 @@ export default function Home() {
 				<div className={styles.banner}>
 					<h3>your business needs memorable branding</h3>
 				</div>
-        <h3 className={`${utilStyles.sectionTitle} ${styles.projectsTitle}`}>featured work</h3>
-        <div className={styles.slider}>
-          <Featured />
-
-        </div>
-        <Link href='/work' className={styles.projectsLink} >All Projects</Link>
+				<h3 className={`${utilStyles.sectionTitle} ${styles.projectsTitle}`}>
+					featured work
+				</h3>
+				<div className={styles.slider}>
+					<Featured />
+				</div>
+				<Link href='/work' className={styles.projectsLink}>
+					All Projects
+				</Link>
 			</section>
 
-      <section className={styles.servicesContainer}>
-        <div className={styles.mobile}>
-          <Service />
-        </div>
-        <div className={styles.desktop}>
-          <ServiceGrid />
-        </div>
-      </section>
+			<section className={styles.servicesContainer}>
+				<div className={styles.mobile}>
+					<Service />
+				</div>
+				<div className={styles.desktop}>
+					<ServiceGrid />
+				</div>
+			</section>
 
-      <section className={styles.contactContainer}>
-        <div className={styles.topInnerContactContainer}>
-          <h3 className={styles.darkGreyBoxContent}>Get in contact</h3>
-        </div>
+			<section className={styles.contactContainer}>
+				<div className={styles.topInnerContactContainer}>
+					<h3 className={styles.darkGreyBoxContent}>Get in contact</h3>
+				</div>
 
-        <div className={styles.darkGreyBox}></div>
-        <div className={styles.bottomInnerContactContainer}>
-          <p>Are you ready to take your business to the next level?</p>
-          <p>Are you ready to take your business to the next level?
-             Please fill out the form below so we can get an idea of your project needs. 
-             Please allow 2 business days to hear back from us
-          </p>
-        <Link href='/services' className={utilStyles.pageLink} >Let's Begin</Link>
-        </div>
-
-      </section>
+				<div className={styles.darkGreyBox}></div>
+				<div className={styles.bottomInnerContactContainer}>
+					<p>Are you ready to take your business to the next level?</p>
+					<p>
+						Are you ready to take your business to the next level? Please fill
+						out the form below so we can get an idea of your project needs.
+						Please allow 2 business days to hear back from us
+					</p>
+					<Link href='/services' className={utilStyles.pageLink}>
+						Let's Begin
+					</Link>
+				</div>
+			</section>
 
 			<footer className={utilStyles.container}>
-        <Info />
+				<Info />
 			</footer>
 		</div>
 	);
