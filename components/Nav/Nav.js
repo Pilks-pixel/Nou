c2302beb2e2101db85b44dec24a9247cb6e14a34
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2023-02-02 13:52:10 +0700

    nav mobile background

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -7,18 +7,18 @@ export default function Nav() {
     return <>
 
     <nav className={styles.nav}>
-      {/* <div className={styles.containerLogo}> */}
-        <Image
-          className={styles.logo}
-          src='/noufiles_large.png'
-          alt='nou logo'
-          width={260}
-          height={150}
-          // style={{
-          //   maxwidth: "200px",
-          //   height: "auto",}}
-        />
-      {/* </div> */}
+      <Image
+        className={styles.logo}
+        src='/noufiles_large.png'
+        alt='nou logo'
+        width={260}
+        height={150}
+        // style={{
+        //   maxwidth: "200px",
+        //   height: "auto",}}
+       />
+
+       <button><span className='srOnly'>Menu</span></button>
       <ul className={styles.containerPrimary}>
         <li> 
           <Link href='/services'>
