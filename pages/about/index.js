60ba31ad38f54fb4acac4b82fc0adc9de97406c2
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2023-02-08 16:02:51 +0700

    about grid refactor

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,52 +1,63 @@
-import Head from "next/head"
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
-import utilStyles from "../../styles/utils.module.css"
+import utilStyles from "../../styles/utils.module.css";
 import Nav from "../../components/Nav/Nav";
-import styles from "../../styles/About.module.css"
-
+import Info from "../../components/Info/Info";
+import styles from "../../styles/About.module.css";
 
 export default function About() {
-  
-    return (
-        <div className={utilStyles.container}>
-        <Head>
-        <title>About</title>
-        
-        </Head>
 
-      <header>
+	return (
+		<div className={utilStyles.container}>
+			<Head>
+				<title>About</title>
+			</Head>
+
+			<header>
 				<Nav />
 			</header>
 
-      <main className={styles.aboutContainer}>
-        <h2 className={styles.title}>About me</h2>
-        <Image
-        className={styles.aboutImg}
-        src='/about_me.png'
-        height={400}
-        width={350}
-        alt='Picture of Horten, Nou Design Founder'
-        >
-        </Image>
-        <section className={styles.aboutInfo}>
-          <p>Hey there, I am Horten, a visual designer specialised in minimalism Branding & Web Design</p>
-            
-          <p>I believe in creating Visual Designs with purpose, that are timeless &</p>
+			<main className={`${utilStyles.container} ${styles.theme}`}>
+				<div className={styles.aboutContainer}>
+					<h2 className={styles.title}>About me</h2>
 
-          <p>She / Her • proud Spanish / British</p>
+					<Image
+						className={styles.aboutImg}
+						src='/about_me.png'
+						height={400}
+						width={350}
+						alt='Picture of Horten, Nou Design Founder'
+					></Image>
 
-          <Link href='/contact'>Say Hello</Link>
-        </section>
+					<section className={styles.aboutInfo}>
+						<p>
+							Hey there, I am Horten, a visual designer specialised in
+							minimalism Branding & Web Design
+						</p>
 
+						<p>
+							I believe in creating Visual Designs with purpose, that are
+							timeless &
+						</p>
 
-      </main>
-      <footer>
+						<p>She / Her • proud Spanish / British</p>
 
-      </footer>
-
-
-    </div>
-  );
-  
+						<Link
+							href='/contact'
+							style={{
+								"text-decoration": "underline",
+							}}
+						>
+							Say Hello
+						</Link>
+					</section>
+				</div>
+			</main>
+      
+			<footer className={utilStyles.container}>
+				<Info />
+			</footer>
+		</div>
+	);
 }
