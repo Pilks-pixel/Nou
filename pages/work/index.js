6e4de1a8a9ee018193b585c2b46860ca8dfc0c36
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2023-03-19 17:01:35 +0700

    projects title

--- pages/work/index.js
+++ pages/work/index.js
@@ -5,6 +5,7 @@ import styles from "../../styles/Work.module.css";
 import utilStyles from "../../styles/utils.module.css";
 import Nav from "../../components/Nav/Nav";
 import Image from "next/image";
+import Info from "../../components/Info/Info"
 
 export default function Work() {
 	return (
@@ -19,106 +20,119 @@ export default function Work() {
 
 			<main className={styles.projectContainer}>
 				<h2 className={styles.mainTitle}>Featured work</h2>
-				<mainion className={styles.projectCard}>
-					<h2 className={styles.projectTitle}>Silk & Sense</h2>
-					<p>Art Direction and Branding</p>
+				<section className={styles.projectCard}>
+					<div className={styles.projectTitle}>
+						<h2>Silk & Sense</h2>
+						<p>Art Direction and Branding</p>
+					</div>
 					<Image
-						className={styles.projectImg}
+						className={styles.mainProjectImg}
 						src='/silk/silk.png'
 						height={400}
 						width={350}
 						alt='Silk and sense logo'
 					></Image>
-					<h3>Premium Brand Specialised In Artisanal Handmade Scarves</h3>
-					<p>
-						The branch on the logo symbolises the family, a mother who started
-						the business (the truck) and the daughter that put that vision into
-						reality (the shoots) The thin lines, the light, neutral colours and
-						the typography define the minimal, feminine and delicate and
-						delicate personality of each scarf
-					</p>
-					<div className={styles.projectImgContainer}>
-						<Image
-							className={styles.projectImg}
-							src='/silk/silk_logo.png'
-							height={400}
-							width={350}
-							alt='Silk and sense submark logo'
-						></Image>
-						<Image
-							className={styles.projectImg}
-							src='/silk/silk_mockup.png'
-							height={400}
-							width={350}
-							alt='Silk and sense logo on a badge'
-						></Image>
-					</div>s				</mainion>
 
-				<mainion className={styles.projectCard}>
-					<h2 className={styles.projectTitle}>Moane</h2>
-					<p>Art Direction and Branding</p>
+					<div className={styles.projectDescription}>
+						<h3>Premium Brand Specialised In Artisanal Handmade Scarves</h3>
+						<p>
+							The branch on the logo symbolises the family, a mother who started
+							the business (the truck) and the daughter that put that vision
+							into reality (the shoots) The thin lines, the light, neutral
+							colours and the typography define the minimal, feminine and
+							delicate and delicate personality of each scarf
+						</p>
+					</div>
 					<Image
 						className={styles.projectImg}
+						src='/silk/silk_logo.png'
+						height={400}
+						width={350}
+						alt='Silk and sense submark logo'
+					></Image>
+					<Image
+						className={styles.projectImg}
+						src='/silk/silk_mockup.png'
+						height={400}
+						width={350}
+						alt='Silk and sense logo on a badge'
+					></Image>
+				</section>
+
+				<section className={styles.projectCard}>
+					<div className={styles.projectTitle}>
+						<h2>Moane</h2>
+						<p>Art Direction and Branding</p>
+					</div>
+					<Image
+						className={styles.mainProjectImg}
 						src='/moane/colours_artboard.png'
 						height={400}
 						width={350}
 						alt='Moane logo'
 					></Image>
-					<p>
-						Moane is a Interior Design Studio, specialised in comfortable ,
-						stylish interiors that represents the beauty of the island of Bali
-					</p>
-					<div className={styles.projectImgContainer}>
-						<Image
-							className={styles.projectImg}
-							src='/moane/moane.png'
-							height={400}
-							width={350}
-							alt='Moane business card'
-						></Image>
-						<Image
-							className={styles.projectImg}
-							src='/moane/mockup.png'
-							height={400}
-							width={350}
-							alt='Moane submark logo'
-						></Image>
-					</div>s				</mainion>
 
-				<section className={styles.projectCard}>
-					<h2 className={styles.projectTitle}>Fresh & Kind</h2>
-					<p>Art Direction and packaging</p>
+					<div className={styles.projectDescription}>
+						<p>
+							Moane is a Interior Design Studio, specialised in comfortable ,
+							stylish interiors that represents the beauty of the island of Bali
+						</p>
+					</div>
 					<Image
 						className={styles.projectImg}
+						src='/moane/moane.png'
+						height={400}
+						width={350}
+						alt='Moane business card'
+					></Image>
+					<Image
+						className={styles.projectImg}
+						src='/moane/mockup.png'
+						height={400}
+						width={350}
+						alt='Moane submark logo'
+					></Image>
+				</section>
+
+				<section className={styles.projectCard}>
+					<div className={styles.projectTitle}>
+						<h2 className={styles.projectTitle}>Fresh & Kind</h2>
+						<p>Art Direction and packaging</p>
+					</div>
+					<Image
+						className={styles.mainProjectImg}
 						src='/fresh/fresh_logo.png'
 						height={400}
 						width={350}
 						alt='fresh and kind logo'
 					></Image>
-					<p>
-						FRESH + KIND is a haircare brand that has a vision of bringing and
-						organic, vegan and natural shampoo range to those who care about the
-						planet without compromising on product quality.
-					</p>
-					<div className={styles.projectImgContainer}>
-						<Image
-							className={styles.projectImg}
-							src='/fresh/mockup2.jpeg'
-							height={400}
-							width={350}
-							alt='fresh and kind logo on smartphone'
-						></Image>
-						<Image
-							className={styles.projectImg}
-							src='/fresh/business_card.jpeg'
-							height={400}
-							width={350}
-							alt='fresh and kind business card'
-						></Image>
+
+					<div className={styles.projectDescription}>
+						<p>
+							FRESH + KIND is a haircare brand that has a vision of bringing and
+							organic, vegan and natural shampoo range to those who care about
+							the planet without compromising on product quality.
+						</p>
 					</div>
+					<Image
+						className={styles.projectImg}
+						src='/fresh/mockup2.jpeg'
+						height={400}
+						width={350}
+						alt='fresh and kind logo on smartphone'
+					></Image>
+					<Image
+						className={styles.projectImg}
+						src='/fresh/business_card.jpeg'
+						height={400}
+						width={350}
+						alt='fresh and kind business card'
+					></Image>
 				</section>
 			</main>
-			<footer></footer>
+			<footer>
+                <Info />
+            </footer>
 		</div>
 	);
 }
